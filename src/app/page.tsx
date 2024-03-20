4d335785d5e71aa36dbe52293958df6734ecbfc3
KyrianTM <r0929774@student.thomasmore.be>
2024-03-20 15:53:26 +0100

    resize

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,8 +33,7 @@ export default function Home() {
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
           src="/TARON.jpg"
           alt="edge of madness"
-          width={180}
-          height={37}
+          width={300}
           priority
         />
       </div>
